960c0568a310172118f151892a404be6f1a01f28
Michael McCarthy <51542091+mccarthy-m-g@users.noreply.github.com>
2021-09-03 12:17:01 -0600

    update assignment custom knit

--- R/knit.R
+++ R/knit.R
@@ -2,16 +2,17 @@ knit_assignment <- function(input, ...) {
 
   yaml_params <- rmarkdown::yaml_front_matter(input)$params
   name <- paste0(yaml_params$lastname, "_", yaml_params$firstname)
+  number <- as.character(yaml_params$assignment)
 
   # Without code
-  filename <- paste0(name, "_Assignment_1.docx")
+  filename <- paste0(name, "_Assignment_", number, ".docx")
   rmarkdown::render(
     input,
     params = list(show_output = FALSE),
     output_file = file.path(dirname(input), filename)
   )
   # With code
-  filename <- paste0(name, "_Assignment_1_OUTPUT.docx")
+  filename <- paste0(name, "_Assignment_", number, "_OUTPUT.docx")
   rmarkdown::render(
     input,
     params = list(show_output = TRUE),
