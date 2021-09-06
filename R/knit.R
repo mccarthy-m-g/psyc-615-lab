d9ba1aea044beabfa0f13478b4a3e737508e97ed
Michael McCarthy <51542091+mccarthy-m-g@users.noreply.github.com>
2021-09-06 14:52:33 -0600

    update custom knit to be more robust and less hacky

--- R/knit.R
+++ R/knit.R
@@ -1,12 +1,45 @@
+#' This function knits both required files for submitting assignments, with the
+#' correct file names and display or hiding of R code. Because students are
+#' using the here package for constructing file paths, the best way to make
+#' this custom knit function work was to reconstruct the entire project in a
+#' temporary directory, modify the temporary version of the input file, then
+#' return the correct output in the assignment folder of the original input. As
+#' a consequence, the speed of this function depends on the size of the project
+#' directory for this lab. Students should be told not to store large files in
+#' it for now.
 knit_assignment <- function(input, ...) {
 
+  # Temporary project ---------------------------------------------------------
+
+  # Get paths to input project root and input file
+  input_proj_root <- here::here()
+
+  # Copy the entire project to a temporary directory
+  temp_proj_root <- withr::local_tempdir()
+  fs::dir_copy(input_proj_root, temp_proj_root)
+  temp_proj_root <- fs::path(fs::as_fs_path(temp_proj_root), "psyc-615-lab")
+
+  # Get path to input file copy in the temporary directory
+  input_rel_path  <- stringr::str_remove(input, input_proj_root)
+  temp_input      <- fs::path(temp_proj_root, fs::as_fs_path(input_rel_path))
+
+  #withr::local_dir(temp_proj_root)
+
+  # Update temp input file ----------------------------------------------------
+
+  # Parse YAML of input for its params. These are used for constructing the
+  # output file name, as well as adding additional YAML parameters before
+  # rendering.
   yaml_params <- rmarkdown::yaml_front_matter(input)$params
   name <- paste0(yaml_params$lastname, "_", yaml_params$firstname)
   number <- as.character(yaml_params$assignment)
 
-  # Add yaml section for title page by appending YAML section to end of the
-  # input Rmd
+  # Read lines of the input so that additions can be made to it behind the
+  # scenes
   input_lines <- stringi::stri_read_lines(input)
+
+  # Add new parameters to the input YAML that will construct the title page of
+  # the rendered output
   input_lines <- c(
     input_lines[1:3],
     "author:",
@@ -22,59 +55,48 @@ knit_assignment <- function(input, ...) {
     "          TAs: `r params$TAs`",
     input_lines[4:length(input_lines)]
   )
-  # Overwrite original Rmd. Note: the reason for overwriting the original
-  # rather than copying to a temporary directory is because the here package
-  # does not play nice with a temporary directory in this situation. Is this
-  # method hackish? Yes, but it works and removes clutter from assignments that
-  # might distract students new to R and R Markdown.
+
+  # Overwrite temp input file with updates
   stringi::stri_write_lines(
     input_lines,
-    con = input
+    con = temp_input
   )
 
-  # Without code --------------------------------------------------------------
+  # Render assignment without code --------------------------------------------
 
   filename <- paste0(name, "_Assignment_", number, ".docx")
   rmarkdown::render(
-    input,
+    temp_input,
     params = list(show_output = FALSE),
     output_file = file.path(dirname(input), filename)
   )
 
-  # With code -----------------------------------------------------------------
+  # Update temp input file again ----------------------------------------------
 
-  # This appends a code chunk to the document that shows all code output
-  # at the end
-  input_echo <- c(
+  # Appends a code chunk to the document that shows all code output at the end
+  input_lines <- c(
     input_lines,
     "",
-    "# Output",
+    "# All R Output",
     "",
     "```{r all-output}",
     "mget(ls())",
     "```"
   )
 
-  # Overwrite original Rmd
+  # Overwrite temp input file with updates
   stringi::stri_write_lines(
-    input_echo,
-    con = input
+    input_lines,
+    con = temp_input
   )
 
-  # Render assignment with code output
+  # Render assignment with code -----------------------------------------------
+
   filename <- paste0(name, "_Assignment_", number, "_OUTPUT.docx")
   rmarkdown::render(
-    input,
+    temp_input,
     params = list(show_output = TRUE),
     output_file = file.path(dirname(input), filename),
     envir = new.env()
-  )
-
-  # Restore original Rmd. Again, a hackish but working solution. First remove
-  # added code chunk, then added YAML.
-  input_lines <- head(stringi::stri_read_lines(input), -6)[-c(4:14)]
-  stringi::stri_write_lines(
-    input_lines,
-    con = input
   )
 }
